e8fb8ed8dba6cd6d8908753340789ab1897423e7
Kaike <kaikedias98@gmail.com>
2025-01-03 15:10:13 -0300

    feat: add menu page

---
+++ src/pages/MenuPage.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+function onCardClick() {
+  console.log('Card clicado!')
+}
+</script>
+
+<template>
+  <div class="q-app q-pa-lg q-gutter-y-lg" style="min-height: 100vh">
+    <q-card elevated clickable class="cursor-pointer q-pa-md" @click="onCardClick">
+      <q-card-section class="row justify-between items-center">
+        <div class="row items-center">
+          <q-icon name="restaurant_menu" size="32px" class="q-mr-md" />
+          <div class="text-h4">Cardápio</div>
+        </div>
+        <q-icon name="navigate_next" size="32px" class="q-mr-md" />
+      </q-card-section>
+    </q-card>
+
+    <q-card elevated clickable class="cursor-pointer q-pa-md" @click="onCardClick">
+      <q-card-section class="row justify-between items-center">
+        <div class="row items-center">
+          <q-icon name="description" size="32px" class="q-mr-md" />
+          <div class="text-h4">Documentos</div>
+        </div>
+        <q-icon name="navigate_next" size="32px" class="q-mr-md" />
+      </q-card-section>
+    </q-card>
+
+    <q-card elevated clickable class="cursor-pointer q-pa-md" @click="onCardClick">
+      <q-card-section class="row justify-between items-center">
+        <div class="row items-center">
+          <q-icon name="assignment" size="32px" class="q-mr-md" />
+          <div class="text-h4">Orientações Nutricionais</div>
+        </div>
+        <q-icon name="navigate_next" size="32px" class="q-mr-md" />
+      </q-card-section>
+    </q-card>
+
+    <q-card elevated clickable class="cursor-pointer q-pa-md" @click="onCardClick">
+      <q-card-section class="row justify-between items-center">
+        <div class="row items-center">
+          <q-icon name="free_cancellation" size="32px" class="q-mr-md" />
+          <div class="text-h4">Diário Alimentar</div>
+        </div>
+        <q-icon name="navigate_next" size="32px" class="q-mr-md" />
+      </q-card-section>
+    </q-card>
+
+    <q-card elevated clickable class="cursor-pointer q-pa-md" @click="onCardClick">
+      <q-card-section class="row justify-between items-center">
+        <div class="row items-center">
+          <q-icon name="pending_actions" size="32px" class="q-mr-md" />
+          <div class="text-h4">Metas</div>
+        </div>
+        <q-icon name="navigate_next" size="32px" class="q-mr-md" />
+      </q-card-section>
+    </q-card>
+  </div>
+</template>
