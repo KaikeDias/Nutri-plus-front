f507804cc578a7aa89ff93dc29408d3de214acfd
Kaike <kaikedias98@gmail.com>
2025-01-03 15:10:02 -0300

    feat: add drawer

---
+++ src/components/DrawerItem.vue
@@ -0,0 +1,29 @@
+<template>
+  <q-item
+    clickable
+    tag="a"
+    target="_blank"
+  >
+    <q-item-section
+      v-if="icon"
+      avatar
+    >
+      <q-icon :name="icon" />
+    </q-item-section>
+
+    <q-item-section>
+      <q-item-label>{{ title }}</q-item-label>
+    </q-item-section>
+  </q-item>
+</template>
+
+<script setup lang="ts">
+export interface DrawerItemProps {
+  title: string;
+  icon?: string;
+};
+
+withDefaults(defineProps<DrawerItemProps>(), {
+  icon: '',
+});
+</script>
