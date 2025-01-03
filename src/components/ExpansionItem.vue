73c151494db822cfe6f2d8c2272f94d89c2c08f5
Kaike <kaikedias98@gmail.com>
2025-01-03 18:48:02 -0300

    feat: add expansion item component

---
+++ src/components/ExpansionItem.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+import type Guideline from 'src/models/guideline'
+
+defineProps<{guideline: Guideline}>()
+
+</script>
+
+<template>
+  <q-expansion-item
+    expand-separator
+    :label="guideline.title"
+    class="shadow-3 overflow-hidden q-mb-md"
+    style="border-radius: 10px"
+  >
+    <q-card>
+      <q-separator />
+      <q-card-section>
+        {{ guideline.content }}
+      </q-card-section>
+    </q-card>
+  </q-expansion-item>
+</template>
