2af77ee30974cff6bc76a45aa5df74cfebcbafae
Kaike <kaikedias98@gmail.com>
2025-01-07 15:00:15 -0300

    feat: add date picker

--- src/layouts/RegisterLayout.vue
+++ src/layouts/RegisterLayout.vue
@@ -2,13 +2,21 @@
 import { ref } from 'vue'
 import logo from 'assets/logo.png'
 
+const dateMask = ref('##/##/####');
+const cpfMask = ref('###.###.###-##');
+
 const username = ref('')
 const email = ref('')
 const password = ref('')
+const confirmPassword = ref('')
 const birthDate = ref('')
 const cpf = ref('')
 const crn = ref('')
 const isPwd = ref(true)
+const isCPwd = ref(true)
+
+
+
 </script>
 
 <template>
@@ -20,32 +28,28 @@ const isPwd = ref(true)
         </div>
         <q-form>
           <div class="row q-gutter-x-md">
-            <q-input
-              outlined
-              v-model="username"
-              label="Username"
-              class="q-mb-md"
-              style="min-width: 350px"
-            >
+            <q-input outlined v-model="username" label="Username" class="q-mb-md" style="min-width: 350px">
               <template v-slot:prepend>
                 <q-icon name="person" />
               </template>
             </q-input>
-            <q-input
-              outlined
-              v-model="birthDate"
-              label="Data de Nascimento"
-              class="q-mb-md"
-              style="min-width: 350px"
-            >
+            <q-input outlined v-model="birthDate" :mask="dateMask" label="Data de Nascimento" class="q-mb-md" style="min-width: 350px">
               <template v-slot:prepend>
-                <q-icon name="calendar_month" />
+                <q-icon name="event" class="cursor-pointer">
+                  <q-popup-proxy cover transition-show="scale" transition-hide="scale">
+                    <q-date mask="DD/MM/YYYY" v-model="birthDate">
+                      <div class="row items-center justify-end">
+                        <q-btn v-close-popup label="Close" color="primary" flat />
+                      </div>
+                    </q-date>
+                  </q-popup-proxy>
+                </q-icon>
               </template>
             </q-input>
           </div>
 
           <div class="row q-gutter-x-md">
-            <q-input outlined v-model="cpf" label="CPF" class="q-mb-md" style="min-width: 350px">
+            <q-input outlined v-model="cpf" label="CPF" :mask="cpfMask" class="q-mb-md" style="min-width: 350px">
               <template v-slot:prepend>
                 <q-icon name="badge" />
               </template>
@@ -56,61 +60,32 @@ const isPwd = ref(true)
               </template>
             </q-input>
           </div>
-          <q-input
-            outlined
-            v-model="email"
-            label="Email"
-            class="q-mb-md"
-            style="min-width: 350px"
-          >
+          <q-input outlined v-model="email" label="Email" class="q-mb-md" style="min-width: 350px">
             <template v-slot:prepend>
               <q-icon name="email" />
             </template>
           </q-input>
 
-          <q-input
-            outlined
-            v-model="password"
-            :type="isPwd ? 'password' : 'text'"
-            label="Senha"
-            class="q-mb-lg"
-          >
+          <q-input outlined v-model="password" :type="isPwd ? 'password' : 'text'" label="Senha" class="q-mb-lg">
             <template v-slot:prepend>
               <q-icon name="lock" />
             </template>
             <template v-slot:append>
-              <q-icon
-                :name="isPwd ? 'visibility_off' : 'visibility'"
-                class="cursor-pointer"
-                @click="isPwd = !isPwd"
-              />
+              <q-icon :name="isPwd ? 'visibility_off' : 'visibility'" class="cursor-pointer" @click="isPwd = !isPwd" />
             </template>
           </q-input>
 
-          <q-input
-            outlined
-            v-model="password"
-            :type="isPwd ? 'password' : 'text'"
-            label="Confirmar Senha"
-            class="q-mb-lg"
-          >
+          <q-input outlined v-model="confirmPassword" :type="isCPwd ? 'password' : 'text'" label="Confirmar Senha"
+            class="q-mb-lg">
             <template v-slot:prepend>
               <q-icon name="lock" />
             </template>
             <template v-slot:append>
-              <q-icon
-                :name="isPwd ? 'visibility_off' : 'visibility'"
-                class="cursor-pointer"
-                @click="isPwd = !isPwd"
-              />
+              <q-icon :name="isCPwd ? 'visibility_off' : 'visibility'" class="cursor-pointer" @click="isCPwd = !isCPwd" />
             </template>
           </q-input>
           <div class="row justify-center">
-            <q-btn
-              color="primary"
-              label="Entrar"
-              style="height: 50px; min-width: 250px; border-radius: 10px"
-            />
+            <q-btn color="primary" label="Cadastrar-se" style="height: 50px; min-width: 250px; border-radius: 10px" />
           </div>
         </q-form>
       </q-card-section>
